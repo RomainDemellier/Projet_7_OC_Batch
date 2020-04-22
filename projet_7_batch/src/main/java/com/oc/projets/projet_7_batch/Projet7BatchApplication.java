e5d9c5899e0a850fd538d2089194e7395d661ccb
Romain <romaindemellier@yahoo.fr>
2020-04-22 15:42:04 +0200

    Création du projet_7_batch

---
+++ projet_7_batch/src/main/java/com/oc/projets/projet_7_batch/Projet7BatchApplication.java
@@ -0,0 +1,13 @@
+package com.oc.projets.projet_7_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet7BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet7BatchApplication.class, args);
+	}
+
+}
